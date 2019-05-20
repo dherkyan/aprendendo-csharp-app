86401fd02a3dd79797f7ffba7b9dd64018cea664
dherkyan <dherkyan@gmail.com>
2019-05-20 08:16:35 -0400

    PRimitivos

---
+++ Primitivos/Primitivos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendendoCSharpApp.Primitivos
+{
+    class Primitivos : IMain
+    {
+        public void Excutar()
+        {
+            bool Bools = true;
+            byte Bytes = 0;
+            int Ints = 0;
+            uint UInts = 0;
+            long Longs = 0;
+            ulong ULongs = 0;
+            float Floats = 0.0f;
+            double Doubles = 0;
+        }
+
+        public void Titulo()
+        {
+            Utilitario.Mensagem("Primitivos básicos");
+        }
+    }
+}
