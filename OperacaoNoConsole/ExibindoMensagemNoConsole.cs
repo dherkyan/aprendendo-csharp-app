2d0aa4945a8dd9059dfaf26c6e9123326298e10f
dherkyan <dherkyan@gmail.com>
2019-05-20 08:18:34 -0400

    Aprendendo operacoes no console

---
+++ OperacaoNoConsole/ExibindoMensagemNoConsole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace AprendendoCSharpApp.OperacaoNoConsole
+{
+    class ExibindoMensagemNoConsole : IMain
+    {
+        public void Excutar()
+        {
+            Titulo();
+
+            Console.Write("Texto sem quebra de linha");
+            Console.WriteLine("Texto com quebra de linnha...");
+        }
+
+        public void Titulo()
+        {
+            Utilitario.Mensagem("Exibindo mensagem no console...");
+        }
+    }
+}
---
+++ OperacaoNoConsole/LendoTecladoNoConsole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendendoCSharpApp.OperacaoNoConsole
+{
+    class LendoTecladoNoConsole : IMain
+    {
+        public void Excutar()
+        {
+            Titulo();
+            Console.Write("Informe se nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Olá, {0}", nome);
+        }
+
+        public void Titulo()
+        {
+            Utilitario.Mensagem("Lendo o teclado no console");
+        }
+    }
+}
