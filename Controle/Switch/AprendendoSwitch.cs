2d888f3063944eb98f6d693bea10170c9af77443
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-05-20 17:41:41 -0400

    Switch

---
+++ Controle/Switch/AprendendoSwitch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendendoCSharpApp.Controle.Switch
+{
+    class AprendendoSwitch : IMain
+    {
+        public void Excutar()
+        {
+            int i = 1;
+
+            switch (i)
+            {
+                case 0:
+                    Console.WriteLine("Case {0}",i);
+                    break;
+                case 1:
+                    Console.WriteLine("Case {0}", i);
+                    break;
+                case 2:
+                    Console.WriteLine("Case {0}", i);
+                    break;
+                default:
+                    Console.WriteLine("Case default", i);
+                    break;
+            }
+        }
+
+        public void Titulo()
+        {
+            Utilitario.Mensagem("Aprendendo switch");
+        }
+    }
+}
