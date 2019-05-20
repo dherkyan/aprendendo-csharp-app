187cf1bf3aac024f5ee6390e8c2d30316c3436d9
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-05-20 17:43:18 -0400

    Menu Sistema

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿using AprendendoCSharpApp.Controle.If;
+using AprendendoCSharpApp.Design;
+using AprendendoCSharpApp.IO;
+using AprendendoCSharpApp.Loop;
 using AprendendoCSharpApp.OperacaoNoConsole;
+using AprendendoCSharpApp.Pacote;
 
 namespace AprendendoCSharpApp
 {
@@ -7,10 +11,23 @@ namespace AprendendoCSharpApp
     {
         static void Main(string[] args)
         {
+            /*
             new ExibindoMensagemNoConsole().Excutar();
             new LendoTecladoNoConsole().Excutar();
             new AprendendoIfElse().Excutar();
             new AprendendoIf().Excutar();
+            new AprendendoFor().Excutar();
+            new AprendendoForeach().Excutar();
+            new AprendendoWhile().Excutar();
+            new AprendendoDoWhile().Excutar();
+            new OperacoesComEnum().Excutar();
+            new AprendendoStrings().Excutar();
+            new LerArquivos().Excutar();
+            new OperacoesComFile().Excutar();
+            */
+            new EscreverArquivo().Excutar();
+            
+
         }
     }
 }
