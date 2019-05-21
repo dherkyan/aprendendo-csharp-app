fe3397b785539d04d383ef27ccb2d6a81db65a27
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-05-21 17:29:10 -0400

    Arrays

---
+++ Arrays/AprendendoArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AprendendoCSharpApp.Arrays
+{
+    class AprendendoArray : ISistema
+    {
+        public void Excutar()
+        {
+
+            int[] numeros = new int[3];
+
+            numeros[0] = 10;
+            numeros[1] = 20;
+            numeros[2] = 30;
+
+            Console.WriteLine("Tamanho array: {0}",numeros.Length);
+
+            foreach (int n in numeros)
+            {
+                Console.WriteLine(n);
+            }
+
+
+            string[] letras = { "A", "B", "C" };
+
+            Console.WriteLine("Tamanho array: {0}", letras.Length);
+
+            foreach (string s in letras)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+
+        public void Titulo()
+        {
+            Utilitario.Mensagem("Aprendendo Array");
+        }
+    }
+}
