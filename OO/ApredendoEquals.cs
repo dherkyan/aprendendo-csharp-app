3ffdbbdfdf691a5d36262af02bd0331f1bc4d9b7
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-05-21 17:27:32 -0400

    Orientação a Objeto

---
+++ OO/ApredendoEquals.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendendoCSharpApp.OO
+{
+    class ApredendoEquals : Sistema
+    {
+        public ApredendoEquals() : base("Aprendendo Equals")
+        {
+
+        }
+
+        public override void Excutar()
+        {
+
+            Carro gol = new Carro();
+            gol.placa = "AAA-1234";
+
+            Carro polo = new Carro();
+            polo.placa = "AAA-1234";
+
+            Console.WriteLine("Gol equals Polo: {0}", gol.Equals(polo));
+
+        }
+
+        public override void Titulo()
+        {
+
+        }
+    }
+}
---
+++ OO/Carro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendendoCSharpApp.OO
+{
+    class Carro
+    {
+        public string placa { get; set; }
+
+        public Carro()
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Carro carro &&
+                   placa == carro.placa;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(placa);
+        }
+    }
+}
