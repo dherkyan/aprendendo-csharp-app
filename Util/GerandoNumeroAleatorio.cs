f23dadcc6e1f0b10eb835d39476b630cd9e1028a
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-06-26 18:36:36 -0400

    Aprendendo a gerar números aleatórios

---
+++ Util/GerandoNumeroAleatorio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendendoCSharpApp.Util
+{
+    class GerandoNumeroAleatorio : ISistema
+    {
+        public void Excutar()
+        {
+            Random random = new Random();
+            for (int i = 0; i < 5;i++)
+            {
+                Console.WriteLine("Numero : {0}", random.Next(1000));
+            }
+        }
+
+        public void Titulo()
+        {
+            Utilitario.Mensagem("GerandoNumeroAleatorio...");
+        }
+    }
+}
