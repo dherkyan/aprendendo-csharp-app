4da2a333d17fde1175491fb9ac689df609d00639
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-06-26 18:38:33 -0400

    Aprendendo formatadores

---
+++ OperacaoNoConsole/ConsoleWriteLineApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendendoCSharpApp.OperacaoNoConsole
+{
+    class ConsoleWriteLineApp : ISistema
+    {
+        public void Excutar()
+        {
+            Console.WriteLine("Standard Numeric Format Specifiers");
+            Console.WriteLine(
+                "(C) Currency: . . . . . . . . {0:C}\n" +
+                "(D) Decimal:. . . . . . . . . {0:D}\n" +
+                "(E) Scientific: . . . . . . . {1:E}\n" +
+                "(F) Fixed point:. . . . . . . {1:F}\n" +
+                "(G) General:. . . . . . . . . {0:G}\n" +
+                "    (default):. . . . . . . . {0} (default = 'G')\n" +
+                "(N) Number: . . . . . . . . . {0:N}\n" +
+                "(P) Percent:. . . . . . . . . {1:P}\n" +
+                "(R) Round-trip: . . . . . . . {1:R}\n" +
+                "(X) Hexadecimal:. . . . . . . {0:X}\n",
+                -123, -123.45f);
+        }
+
+        public void Titulo()
+        {
+            Utilitario.Mensagem("Console método WriteLine...");
+        }
+    }
+}
