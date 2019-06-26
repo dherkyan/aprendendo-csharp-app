ecf53dbdc3af25ac58a969ada0092ae8146535dd
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-06-26 18:34:26 -0400

    Aprendendo For in

---
+++ Loop/AprendendoForIn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendendoCSharpApp.Loop
+{
+    class AprendendoForIn
+    {
+        public static void Mainx(string[] args)
+        {
+            Span<int> numeros = new int[] { 1, 5, 15, 20, 40 };
+            foreach (int numero in numeros)
+            {
+                Console.Write($"{numero} ");
+            }
+        }
+    }
+}
