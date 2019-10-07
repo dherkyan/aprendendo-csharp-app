7cbc59a9069de127494eea821a1e9873fdae9a3e
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-10-07 16:25:57 -0400

    Add Metodos

---
+++ OO/Metodos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendendoCSharpApp.OO
+{
+    public class Metodos
+    {
+
+        public int Multiplicar(int x, int y)
+        {
+            return x * y;
+        }
+
+        public double Multiplicar(double x, double y)
+        {
+            return x * y;
+        }
+
+    }
+}
