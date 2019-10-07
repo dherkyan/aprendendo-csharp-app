974cedf9c81c1f4bb202fcccc10700dc737db290
dherkyan <dherkyan@gmail.com>
2019-10-07 06:23:58 -0400

    Exceções customizadas

---
+++ Exceptions/EmailException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AprendendoCSharpApp.Exceptions
+{
+    [Serializable]
+    public class EmailException : System.Exception
+    {
+        // Construtor padrao
+        public EmailException() : base() { }
+
+        // Construtor para mensagem
+        public EmailException(string message) : base(message) { }
+
+        // Construtor para mensagem e exception
+        public EmailException(string message, Exception exception) : base(message, exception) { }
+
+        // Construtor para serializacao
+        protected EmailException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
